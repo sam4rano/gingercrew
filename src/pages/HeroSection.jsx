71613bbc0461f789424f201ef14916c2c3f99a50
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-04-19 00:53:34 +0100

    statistics

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -25,27 +25,27 @@ const HeroSection = () => {
             idx={15}
           />
         </h1> */}
-        <h1 className="text-[64px] font-bold mb-4 p-md text-center">
+        <h1 className="text-[45px] font-bold mb-4 p-md text-center mx-auto w-1/2">
           WE TRANSFORM YOUR IDEA INTO POSSIBLE REALITIES
         </h1>
-        <h1 className="text-[24px]  mb-4 p-md text-center leading-4">
+        <h1 className="text-[20px]  mb-4 p-md text-center leading-4">
           Our Client love to work with us
           <span className="font-bold pl-1">GINGER</span>CREW
         </h1>
       </div>
-      <div className="flex flex-row h-[350px] justify-around">
+      <div className="flex flex-row h-[350px] justify-between">
         <div className="flex flex-row">
-          <img src={imageOne} alt="logo" className="pr-4 rounded-lg" />
-          <img src={imageTwo} alt="logo" className=" rounded-lg" />
+          <img src={imageOne} alt="logo" className="pr-4 rounded-sm w-[400px]" />
+          <img src={imageTwo} alt="logo" className=" rounded-sm" />
         </div>
-        <div className=" p-2">
+        <div className=" p-2 mr-[150px]">
           <div className="flex flex-col text-[24px] pb-2">
             <h1>
               <span className="font-bold">GINGER</span>CREW
             </h1>
             <br />
             <h1 className="max-w-[390px] text-[24px]">
-              is a strategic software development agnecy focused on creation of
+              is a strategic software development agency focused on creation of
               technology products for businesses of every level
             </h1>
           </div>
--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const statistics = () => {
   return (
-    <div className="h-[800px] bg-[#1E1E1E]">
-      <div > 
-        <div className="flex flex-row justify-around pt-[20px] pb-[40px]">
+    <div className="h-[1200px] bg-[#1E1E1E]">
+      <div>
+        <div className="flex flex-row justify-around pt-[20px] pb-[40px] m-[50px]">
           <div>
             <h1 className="font-bold text-[80px]">20</h1>
             <h1>Happy clients</h1>
@@ -18,12 +18,25 @@ const statistics = () => {
             <h1>Completed Projects</h1>
           </div>
         </div>
-        <h1 className='text-white text-[24px] text-center p-8'>
+        <hr />
+        <h1 className="text-white text-[24px] text-center p-8 flex items-center mx-auto h-[72px] w-[1144px] m-[50px]">
           By staying connected with us, we can make your product idea come to
           reality more quickly with our efficient work rates on creation of
           technology products for businesses of every level
         </h1>
-        <div></div>
+        <hr />
+
+        <div className="mx-auto w-[1140px] h-[500px] bg-[#FFFFFF] m-[50px] pb-[50px]"></div>
+        <div className="flex flex-row justify-around">
+          <h1 className="text-[24px] w-[630px]">
+            We build digital products that are world-class. We help deliver
+            value efficiently with our dedicated teams with the right creative
+            and technical blend
+          </h1>
+          <button className="bg-white hover:bg-black hover:text-white text-black font-bold py-2 px-4 rounded w-[200px] text-md h-[50px]">
+            About Us
+          </button>
+        </div>
       </div>
     </div>
   );
